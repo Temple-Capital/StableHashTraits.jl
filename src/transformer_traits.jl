12ef12c9b2018a3f75f3e540f3fccc4519496cc9
Jishnu Bhattacharya <jishnu@temple.capital>
2026-01-08 16:26:23 +0000

    Fix instances of the state not being returned

--- src/transformer_traits.jl
+++ src/transformer_traits.jl
@@ -155,7 +155,7 @@ _transformer_typeof(::UnionAll, context) = transformer(UnionAll, context)
 function hash_type_and_value(::ComputeHash, x, hash_state, context)
     transform = _transformer_typeof(x, context)::Transformer
     tx = transform(x)
-    hash_type_and_value(TraversalStyle(context), x, hash_state, context, transform, tx)
+    hash_state = hash_type_and_value(TraversalStyle(context), x, hash_state, context, transform, tx)
     return hash_state
 end
 
@@ -383,10 +383,13 @@ end
 
 Base.@constprop :aggressive function hash_fields(x, fields, hash_state, context)
     vals = map(field -> getfield(x, field), fields)
-    map(fields, vals) do field, val
+    fT = map(field -> fieldtype(typeof(x), field), fields)
+    fv = ntuple(length(fields)) do i
+        (vals[i], fT[i])
+    end
+    hash_state = foldl(fv, init=hash_state) do hash_state, (val, FT)
         # can we optimize away the field's type_hash?
         transform = transformer(typeof(val), context)
-        FT = fieldtype(typeof(x), field)
         if isconcretetype(FT) && transform.hoist_type && HashRetrievalStrategy(FT) !== FetchHash()
             # the fieldtype has been hashed as part of the type of the container
             hash_value(transform(val), hash_state, context, transform)
@@ -661,7 +664,7 @@ end
 
 function stable_hash_helper(x::Tuple, hash_state, context, ::StructTypes.ArrayType)
     return stable_hash_helper_nested(hash_state) do nested_hash_state
-        nested_hash_state = hash_fields(x, fieldnames(typeof(x)), nested_hash_state, context)
+        hash_fields(x, fieldnames(typeof(x)), nested_hash_state, context)
     end
 end
 
